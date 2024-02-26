74dbb3db7119deb4f5f853892d54563124e9e925
Hinson7886 <JonHinsonfx@gmail.com>
2024-02-26 01:05:09 -0500

    inputs in position

--- Capstone.2310.project/src/Components/Home/Home.jsx
+++ Capstone.2310.project/src/Components/Home/Home.jsx
@@ -1,5 +1,8 @@
 import './home.scss'
 import video from '../../assets/video.mp4'
+import { GrLocation } from 'react-icons/gr'
+
+
 function Home() {
 
     return (
@@ -9,17 +12,45 @@ function Home() {
 
             <div className="homeContent container">
                 <div className="textDiv">
-
                     <span className='smallText'>
                         Our Packages
                     </span>
-
                     <h1 className="homeTitle">
                         Search Your Holiday
                     </h1>
+                </div>
 
+                <div className="cardDiv grid">
+                    <div className="destinationInput">
+                        <label htmlFor="city">Search your destination</label>
+                        <div className="input flex">
+                            <input type="text" placeholder='Enter name here...' />
+                            <GrLocation className="icon" />
+                        </div>
+                    </div>
+
+                    <div className="dateInput">
+                        <label htmlFor="date">Select your date</label>
+                        <div className="input flex">
+                            <input type="date" />
+                            <GrLocation className="icon" />
+                        </div>
+                    </div>
+
+                    <div className="priceInput">
+                        <div className="label_total flex">
+                            <label htmlFor="price">Max price:
+                            </label>
+                            <h3 className="total">$5000</h3>
+                        </div>
+                        <div className="input flex">
+                            <input type="range" max="5000" min="1000" />
+                        </div>
+                    </div>
                 </div>
             </div>
+
+
         </section>
     )
 }
